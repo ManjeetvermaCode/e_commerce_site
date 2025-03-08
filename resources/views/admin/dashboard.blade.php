033e9f46ad74647567608c77d2d21ff384c697b0
manjeetverma <manjeetverma@gmail.com>
2025-03-08 15:48:20 +0530

    admin layout setup done.

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.main')
+
+@section('content')
+<h1>Dashboard</h1>
+    <p>This is my body content Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit, labore! Placeat possimus beatae iure, deleniti veniam cupiditate perferendis nemo optio nulla dolores consequatur quia ex non at distinctio labore quod? lorem&5
+        . Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, debitis aliquid fuga dicta adipisci animi harum eius deleniti officiis recusandae, quasi hic aut provident perferendis dignissimos nemo! Tempora, magnam incidunt!
+        Rem labore, fugit vel id beatae ex laudantium, quis neque rerum, animi ipsa. Rerum hic cum perferendis! Dolor deleniti eaque, accusamus sapiente rem error officiis temporibus iste corrupti, incidunt dicta.
+        Labore modi eius saepe libero provident itaque sed facilis porro voluptatum maxime culpa recusandae suscipit, velit, veritatis neque exercitationem assumenda veniam similique corrupti quam reprehenderit praesentium. Hic officia perferendis facere.
+        Quis eius culpa et officia laborum tenetur? Facilis minima dolorum quam. Accusantium nulla ex consequatur! Enim consequatur eos soluta vitae deserunt. Reprehenderit officiis sunt odit repudiandae explicabo assumenda minima ipsa.
+        Laudantium atque at recusandae laborum modi qui expedita labore sed inventore, similique fuga voluptatibus. Minima mollitia ratione, quod minus in labore totam rerum, dicta dolore dolores accusantium adipisci soluta iste!</p>
+@endsection
---
+++ resources/views/components/admin_footer.blade.php
@@ -0,0 +1,3 @@
+<div class="card fixed-bottom container" style="margin-top:100%">
+      <p>Footer</p>
+</div>
